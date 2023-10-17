f23d7a69c5a72b8f48beaf8405b8f7344882ab55
Dennis Kroné <dennis.krone@svenskaspel.se>
2023-10-17 17:11:25 +0200

    css styling

--- knappen/static/js/script.js
+++ knappen/static/js/script.js
@@ -1,66 +1,66 @@
-const knappen = document.getElementById("knappen");
+// const knappen = document.getElementById("knappen");
 const hunger = document.getElementById("hunger");
 const energi = document.getElementById("energi");
 const gos = document.getElementById("gos");
 const ångest = document.getElementById("ångest");
 
-knappen.addEventListener('pointerdown', function (e) {
+// knappen.addEventListener('pointerdown', function (e) {
 
-    fetch('/knappen', {
-        headers : {
-            'Content-Type' : 'application/json'
-        },
-        method : 'PUT',
-        body : JSON.stringify( {
-            'status' : true
-        })
-    })
-    .then(function (response){
+//     fetch('/knappen', {
+//         headers : {
+//             'Content-Type' : 'application/json'
+//         },
+//         method : 'PUT',
+//         body : JSON.stringify( {
+//             'status' : true
+//         })
+//     })
+//     .then(function (response){
 
-        if(response.ok) {
-            response.json()
-            .then(function(response) {
-                console.log(response);
-            });
-        }
-        else {
-            throw Error('Something went wrong');
-        }
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
-});
+//         if(response.ok) {
+//             response.json()
+//             .then(function(response) {
+//                 console.log(response);
+//             });
+//         }
+//         else {
+//             throw Error('Something went wrong');
+//         }
+//     })
+//     .catch(function(error) {
+//         console.log(error);
+//     });
+// });
 
-var deClickKnappen = function (e) {
+// var deClickKnappen = function (e) {
 
-    fetch('/knappen', {
-        headers : {
-            'Content-Type' : 'application/json'
-        },
-        method : 'PUT',
-        body : JSON.stringify( {
-            'status' : false
-        })
-    })
-    .then(function (response){
+//     fetch('/knappen', {
+//         headers : {
+//             'Content-Type' : 'application/json'
+//         },
+//         method : 'PUT',
+//         body : JSON.stringify( {
+//             'status' : false
+//         })
+//     })
+//     .then(function (response){
 
-        if(response.ok) {
-            response.json()
-            .then(function(response) {
-                console.log(response);
-            });
-        }
-        else {
-            throw Error('Something went wrong');
-        }
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
-};
-knappen.addEventListener('pointerup', deClickKnappen)
-knappen.addEventListener('pointercancel', deClickKnappen);
+//         if(response.ok) {
+//             response.json()
+//             .then(function(response) {
+//                 console.log(response);
+//             });
+//         }
+//         else {
+//             throw Error('Something went wrong');
+//         }
+//     })
+//     .catch(function(error) {
+//         console.log(error);
+//     });
+// };
+// knappen.addEventListener('pointerup', deClickKnappen)
+// knappen.addEventListener('pointercancel', deClickKnappen);
 
 hunger.oninput = function() {
     fetch('/needs/2', {
@@ -169,3 +169,65 @@ gos.oninput = function() {
         console.log(error);
     });
 }
+
+// var scaleCurve = mojs.easing.path('M0,100 L25,99.9999983 C26.2328835,75.0708847 19.7847843,0 100,0');
+//     var el = document.querySelector('.heart-button'),
+//     // mo.js timeline obj
+//     timeline = new mojs.Timeline(),
+
+//     // tweens for the animation:
+
+//     // burst animation
+//     tween1 = new mojs.Burst({
+//         parent: el,
+//     radius:   { 0: 100 },
+//     angle:    { 0: 45 },
+//     y: -10,
+//     count:    10,
+//     radius:       100,
+//     children: {
+//     shape:        'circle',
+//     radius:       30,
+//     fill:         [ 'red', 'white' ],
+//     strokeWidth:  15,
+//     duration:     500,
+//     }
+//     });
+
+
+//     tween2 = new mojs.Tween({
+//         duration : 900,
+//         onUpdate: function(progress) {
+//             var scaleProgress = scaleCurve(progress);
+//             el.style.WebkitTransform = el.style.transform = 'scale3d(' + scaleProgress + ',' + scaleProgress + ',1)';
+//         }
+//     });
+//             tween3 = new mojs.Burst({
+//         parent: el,
+//     radius:   { 0: 100 },
+//     angle:    { 0: -45 },
+//     y: -10,
+//     count:    10,
+//     radius:       125,
+//     children: {
+//     shape:        'circle',
+//     radius:       30,
+//     fill:         [ 'white', 'red' ],
+//     strokeWidth:  15,
+//     duration:     400,
+//     }
+//     });
+
+// // add tweens to timeline:
+// timeline.add(tween1, tween2, tween3);
+
+
+// // when clicking the button start the timeline/animation:
+// $( ".heart-button" ).click(function() {
+//     if ($(this).hasClass('active')){
+//         $(this).removeClass('active');
+//     }else{
+//     timeline.play();
+//     $(this).addClass('active');
+//     }
+// });
