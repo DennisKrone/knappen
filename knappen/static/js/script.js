0cec902b9483ce0f4c9e7fdaf3f444f2c910780d
Dennis Krone <dennis.krone@gmail.com>
2023-10-21 16:38:03 +0200

    Make the heart turn red and send ws event when clicked

--- knappen/static/js/script.js
+++ knappen/static/js/script.js
@@ -170,64 +170,60 @@ gos.oninput = function() {
     });
 }
 
-// var scaleCurve = mojs.easing.path('M0,100 L25,99.9999983 C26.2328835,75.0708847 19.7847843,0 100,0');
-//     var el = document.querySelector('.heart-button'),
-//     // mo.js timeline obj
-//     timeline = new mojs.Timeline(),
 
-//     // tweens for the animation:
+// when clicking the button start the timeline/animation:
+$( ".heart-button" ).click(function() {
+    if ($(this).hasClass('active')){
+        $(this).removeClass('active');
+        fetch('/knappen', {
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            method : 'PUT',
+            body : JSON.stringify( {
+                'status' : false
+            })
+        })
+    .then(function (response){
 
-//     // burst animation
-//     tween1 = new mojs.Burst({
-//         parent: el,
-//     radius:   { 0: 100 },
-//     angle:    { 0: 45 },
-//     y: -10,
-//     count:    10,
-//     radius:       100,
-//     children: {
-//     shape:        'circle',
-//     radius:       30,
-//     fill:         [ 'red', 'white' ],
-//     strokeWidth:  15,
-//     duration:     500,
-//     }
-//     });
+        if(response.ok) {
+            response.json()
+            .then(function(response) {
+                console.log(response);
+            });
+        }
+        else {
+            throw Error('Something went wrong');
+        }
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+    }else{
+    $(this).addClass('active');
+    fetch('/knappen', {
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        method : 'PUT',
+        body : JSON.stringify( {
+            'status' : true
+        })
+    })
+    .then(function (response){
 
-
-//     tween2 = new mojs.Tween({
-//         duration : 900,
-//         onUpdate: function(progress) {
-//             var scaleProgress = scaleCurve(progress);
-//             el.style.WebkitTransform = el.style.transform = 'scale3d(' + scaleProgress + ',' + scaleProgress + ',1)';
-//         }
-//     });
-//             tween3 = new mojs.Burst({
-//         parent: el,
-//     radius:   { 0: 100 },
-//     angle:    { 0: -45 },
-//     y: -10,
-//     count:    10,
-//     radius:       125,
-//     children: {
-//     shape:        'circle',
-//     radius:       30,
-//     fill:         [ 'white', 'red' ],
-//     strokeWidth:  15,
-//     duration:     400,
-//     }
-//     });
-
-// // add tweens to timeline:
-// timeline.add(tween1, tween2, tween3);
-
-
-// // when clicking the button start the timeline/animation:
-// $( ".heart-button" ).click(function() {
-//     if ($(this).hasClass('active')){
-//         $(this).removeClass('active');
-//     }else{
-//     timeline.play();
-//     $(this).addClass('active');
-//     }
-// });
+        if(response.ok) {
+            response.json()
+            .then(function(response) {
+                console.log(response);
+            });
+        }
+        else {
+            throw Error('Something went wrong');
+        }
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+    }
+});
